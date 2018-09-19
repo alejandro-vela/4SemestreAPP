c043b9191801f294b0f313ea36ae2c4488a5fc73
alejandro-vela <31568517+alejandro-vela@users.noreply.github.com>
2018-09-19 00:12:49 -0500

    Create Luchador.java

---
+++ Luchador.java
@@ -0,0 +1,38 @@
+
+
+
+public class Luchador {
+private int vida;
+	private int fuerza;
+	private String nombre = "makio";
+	
+        
+        
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre ;
+	}
+        
+        
+        
+	public int getVida() {
+		return vida;
+	}
+	public void setVida(int vida) {
+		this.vida = vida ;
+	}
+       
+        
+        
+        
+	public int getFuerza() {
+		return fuerza;
+	}
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza ;
+	}
+
+   
+}
